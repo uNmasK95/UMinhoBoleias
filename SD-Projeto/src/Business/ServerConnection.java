21e157250dee4020063d33cfef945096d78dac88
OctavioMaia <octaviojmaia@gmail.com>
2015-12-30 14:22:36 +0000

    update

--- SD-Projeto/src/Business/ServerConnection.java
+++ SD-Projeto/src/Business/ServerConnection.java
@@ -36,52 +36,86 @@ public class ServerConnection implements Runnable{
 		this.in=new BufferedReader(new InputStreamReader(sock.getInputStream()));
     }
     
+    private void regista() throws IOException{
+		String info = in.readLine();
+		String[] arr = info.split(":");
+		
+		
+		if(umb.registaUtilizador(arr[0], arr[1])){ //conseguiu
+			out.write("OK");
+			out.newLine();
+			out.flush();
+		}else{ //falhou
+			out.write("KO");
+			out.newLine();
+			out.flush();
+		}
+    }
+    
     private void login() throws IOException{
 		String info = in.readLine();
 		String[] arr = info.split(":");
 		
-		if(umb.autenticar(arr[0], arr[1])){
-			login = umb.getUser(arr[0]);
+		if(umb.autenticar(arr[0], arr[1])){ //autenticou
+			login = umb.getUser(arr[0]);	//atribui sessao
 			out.write("OK");
 			out.newLine();
 			out.flush();
 		}else{
-			out.write("KO");
+			out.write("KO");				//falhou
 			out.newLine();
 			out.flush();
 		}
     }
-    
-	private void regista() throws IOException{
-		String info = in.readLine();
-		String[] arr = info.split(":");
-		
-		if(umb.registaUtilizador(arr[0], arr[1])){
-			out.write("OK");
-			out.newLine();
-			out.flush();
-		}else{
-			out.write("KO");
-			out.newLine();
-			out.flush();
-		}
-    }
-    
+        
 	private void solicita() throws IOException{
 		String info = in.readLine();
 		String[] arr = info.split(":");
 		Local partida = new Local(arr[1]);
 		Local destino = new Local(arr[2]);
-		login.login(login.getPw(), false, null, partida, in, out, destino);
+		int sleep;
 		
-		out.write(umb.solicitarViagem(arr[0], partida, destino));
+		login.login(login.getPw(), false, null, partida, in, out, destino); //login
+		String aux = umb.solicitarViagem(arr[0], partida, destino);			//string return apos solicitar viagem
+		String[] arr2 = aux.split(":");
+		
+		if(arr2.length==3){													//condutor ja está lá
+			sleep = 0;
+		}else{
+			sleep = Integer.parseInt(arr2[3]);								//condutor está a caminho e demora arr2[3]
+		}
+		
+		out.write(aux);
 		out.newLine();
 		out.flush();
+		
+		try {
+			Thread.sleep(sleep*60000);										//sleep * 60 * 1000 (min * milisegundos)
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		out.write("OK");
+		out.newLine();
+		out.flush();
+		
+		login.getPar().getOut().write("OK");								//informa o cliente que conseguiu solicitar
+		login.getPar().getOut().newLine();
+		login.getPar().getOut().flush();
 	}
 	
 	private void disponivel() throws IOException{
 		String info = in.readLine();
 		String[] arr = info.split(":");
+		Utilizador cliente = umb.getUser(arr[0]);
+		Local l = new Local(arr[1]);
+		String matricula = arr[2];
+		String modelo = arr[3];
+		double custoUnitario = Double.parseDouble(arr[4]);
+		
+		String aux = umb.disponivelViagem(cliente.getEmail(), l, matricula, modelo, custoUnitario);
+		
+		
 	}
 	
     @Override
