77bda71d87a99c25c1c1636341ca41f75e66f15c
uNmasK95 <rui4825@gmail.com>
2015-12-30 18:00:50 +0000

    cliente

--- SD-Projeto/src/Business/ClienteMain.java
+++ SD-Projeto/src/Business/ClienteMain.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 public class ClienteMain implements UMinhoBoleiasIface {
 
 	private String email;
-	private boolean condutor; 
+	private boolean condutor;
 	private Socket sock;
 	private BufferedWriter out;
 	private BufferedReader in;
@@ -47,6 +47,7 @@ public class ClienteMain implements UMinhoBoleiasIface {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	@Override
 	public boolean registaUtilizador(String user, String pass) {
 		String l;
@@ -58,6 +59,7 @@ public class ClienteMain implements UMinhoBoleiasIface {
 			out.flush();
 			l = in.readLine();
 			if (l.equals(KO)) {
+				System.out.println("ERRO-User name já existente");
 				return false;
 			}
 		} catch (IOException ex) {
@@ -70,13 +72,14 @@ public class ClienteMain implements UMinhoBoleiasIface {
 	public boolean autenticar(String user, String pass) {
 		String l;
 		try {
-			out.write(REGISTAUTILIZADOR);
+			out.write(AUTENTICAR);
 			out.newLine();
 			out.write(user + ":" + pass);
 			out.newLine();
 			out.flush();
 			l = in.readLine();
 			if (l.equals(KO)) {
+				System.out.println("ERRO-Credenciais");
 				return false;
 			} else {
 				this.email = user;
@@ -96,10 +99,9 @@ public class ClienteMain implements UMinhoBoleiasIface {
 			out.write(user + ":" + partida.toString() + ":" + destino.toString());
 			out.newLine();
 			out.flush();
-			
-			
+
 			// foi enviado o pedido para o serverconnection
-			
+
 			System.out.println("A espera de um condutor para efetuar a sua viagem");
 			// vai ficar a espera de uma resposta por parte do servidor
 			linhaResposta = in.readLine(); // user:matricula:modelo:?tempo
@@ -122,7 +124,7 @@ public class ClienteMain implements UMinhoBoleiasIface {
 					System.out.println("O Utilizador " + arr[0] + " com o carro de matricula " + arr[1] + " e modelo "
 							+ arr[2] + " já chegou ao local de partida");
 					System.out.println("Foi iniciada a viagem para o destino " + destino.toString());
-				}else{
+				} else {
 					System.out.println("Foi iniciada a viagem para o destino " + destino.toString());
 				}
 			}
@@ -135,7 +137,7 @@ public class ClienteMain implements UMinhoBoleiasIface {
 		}
 		return "Viagem realizada com sucesso";
 	}
-	
+
 	@Override
 	public String disponivelViagem(String user, Local actual, String matricula, String modelo, double custoUnitario) {
 		String linhaResposta;
@@ -150,13 +152,15 @@ public class ClienteMain implements UMinhoBoleiasIface {
 
 			linhaResposta = in.readLine();
 			String[] arr = linhaResposta.split(":");
-			
+
 			if (arr.length == 3) {
 				// tem uma viagem para comprir apartir do local de onte está
-				System.out.println("O Utilizador " + arr[0] + " encontra-se  no loca em que se encontra " + arr[1] + " a espera de boleia para o destino " + arr[2]);
+				System.out.println("O Utilizador " + arr[0] + " encontra-se  no loca em que se encontra " + arr[1]
+						+ " a espera de boleia para o destino " + arr[2]);
 			} else {
 				// o condutor nao se encontra no local de partida
-				System.out.println("O Utilizador " + arr[0] + " encontra-se  no loca " + arr[1] + " a espera de boleia para o destino " + arr[2]);
+				System.out.println("O Utilizador " + arr[0] + " encontra-se  no loca " + arr[1]
+						+ " a espera de boleia para o destino " + arr[2]);
 			}
 
 			linhaResposta = in.readLine();
@@ -167,11 +171,11 @@ public class ClienteMain implements UMinhoBoleiasIface {
 				if (arr.length == 4) {
 					System.out.println("Já se encontra no local de partida " + arr[1]);
 					System.out.println("Foi iniciada a viagem para o destino " + arr[2]);
-				}else{
+				} else {
 					System.out.println("Foi iniciada a viagem para o destino " + arr[2]);
 				}
 			}
-			
+
 			// apos o serverconnection ter fetio o sleep do tempo de viagem
 			// tem de receber o custo
 			linhaResposta = in.readLine();
@@ -179,7 +183,7 @@ public class ClienteMain implements UMinhoBoleiasIface {
 		} catch (IOException ex) {
 			return "Viagem nao realizada";
 		}
-		return "Viagem realizada com sucesso";			
+		return "Viagem realizada com sucesso";
 	}
 
 	@Override
@@ -188,34 +192,35 @@ public class ClienteMain implements UMinhoBoleiasIface {
 			out.write(LOGOUT);
 			out.newLine();
 			out.flush();
+			this.email = null;
 		} catch (IOException e) {
 			System.out.println("ERRO fazer logout");
 			e.printStackTrace();
-		}	
+		}
 	}
 
 	private static String menuInicio() {
 		StringBuilder ap = new StringBuilder();
-		ap.append("=========Menu=========");
-		ap.append("0-Sair");
-		ap.append("1-Registar utilizador");
-		ap.append("2-Login Utilizador");
+		ap.append("=========Menu=========\n");
+		ap.append("0-Sair\n");
+		ap.append("1-Registar utilizador\n");
+		ap.append("2-Login Utilizador\n");
 		return ap.toString();
 	}
-	
-	private static String menuCondutor(){
+
+	private static String menuCondutor() {
 		StringBuilder ap = new StringBuilder();
-		ap.append("=========Menu Condutor=========");
-		ap.append("1-Disponibilizar boleia");
-		ap.append("Outro-Fazer logout");
+		ap.append("=========Menu Condutor=========\n");
+		ap.append("1-Disponibilizar boleia\n");
+		ap.append("Outro-Fazer logout\n");
 		return ap.toString();
 	}
-	
-	private static String menuCliente(){
+
+	private static String menuCliente() {
 		StringBuilder ap = new StringBuilder();
-		ap.append("=========Menu Cliente=========");
-		ap.append("1-Disponibilizar boleia");
-		ap.append("Outro-Fazer logout");
+		ap.append("=========Menu Cliente=========\n");
+		ap.append("1-Solicitar boleia\n");
+		ap.append("Outro-Fazer logout\n");
 		return ap.toString();
 	}
 
@@ -226,98 +231,102 @@ public class ClienteMain implements UMinhoBoleiasIface {
 		int y = lerint();
 		return new Local(x, y);
 	}
-	
+
 	private static int lerint() {
-		Integer ret =0;
+		Integer ret = 0;
 		String inp = input.nextLine();
-		try{
+		try {
 			ret = Integer.parseInt(inp);
-		}catch(Exception e){
-			ret = lerint();			
+		} catch (Exception e) {
+			ret = lerint();
 		}
 		return ret;
 	}
-	
+
 	public static void main(String[] args) {
 		int port;
-		try{
+		try {
 			port = Integer.parseInt(args[1]);
-		}catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("ERRO ao Ler porta");
-			port=lerint();
+			port = lerint();
 		}
 		ClienteMain c1 = new ClienteMain(args[0], 6969);
 		int n;
-		
+
 		System.out.println(menuInicio());
-		while ((n = lerint()) != 0 && (c1.email)==null) {
-			System.out.println(menuInicio());
+		while ((n = lerint()) != 0) {
 			switch (n) {
-				case 1: {
-					System.out.println("=========Registar=========");
-					System.out.println("Email");
-					String email = input.nextLine();
-					System.out.println("Password");
-					String pass = input.nextLine();
-					c1.registaUtilizador(email, pass);
-					break;
-				}
-				case 2: {
-					System.out.println("=========LOGIN=========");
-					System.out.println("Email");
-					String email = input.nextLine();
-					System.out.println("Password");
-					String pass = input.nextLine();
-					c1.autenticar(email, pass);
+			case 1: {
+				System.out.println("=========Registar=========");
+				System.out.println("Email");
+				String email = input.nextLine();
+				System.out.println("Password");
+				String pass = input.nextLine();
+				c1.registaUtilizador(email, pass);
+				break;
+			}
+			case 2: {
+				System.out.println("=========LOGIN=========");
+				System.out.println("Email");
+				String email = input.nextLine();
+				System.out.println("Password");
+				String pass = input.nextLine();
+				boolean ret = c1.autenticar(email, pass);
+				if(ret){
 					System.out.println("1-Condutor outro-Cliente");
-					if((n = lerint())==1){
+					if ((n = lerint()) == 1) {
 						c1.condutor = true;
-					}else{
+					} else {
 						c1.condutor = false;
 					}
-					break;
 				}
-				default:
-					System.out.println("Insira um numero do menu");
+				break;
 			}
-			if(c1.condutor){
-				System.out.println(menuCondutor());
-				if((n = lerint()) == 1) {
-					System.out.println("=========Disponibilizar Viagem=========");
-					if (c1.getEmail() != null) {
-						System.out.println("Local de Atual");
-						Local atual = lerLocal();
-						System.out.println("Matricula : __-__-__");
-						String matricula = input.nextLine();
-						System.out.println("Modelo :");
-						String modelo = input.nextLine();
-						System.out.println("Custo da viagem :");
-						double custoUnitario = input.nextDouble();
-						c1.disponivelViagem(c1.getEmail(), atual, matricula, modelo, custoUnitario);
+			default:
+				System.out.println("Insira um numero do menu");
+			}
+			if (c1.email != null) {
+				if (c1.condutor) {
+					System.out.println(menuCondutor());
+					if ((n = lerint()) == 1) {
+						System.out.println("=========Disponibilizar Viagem=========");
+						if (c1.getEmail() != null) {
+							System.out.println("Local de Atual");
+							Local atual = lerLocal();
+							System.out.println("Matricula : __-__-__");
+							String matricula = input.nextLine();
+							System.out.println("Modelo :");
+							String modelo = input.nextLine();
+							System.out.println("Custo da viagem :");
+							double custoUnitario = input.nextDouble();
+							c1.disponivelViagem(c1.getEmail(), atual, matricula, modelo, custoUnitario);
+						} else {
+							System.out.println("Necessita de estar autenticado");
+						}
 					} else {
-						System.out.println("Necessita de estar autenticado");
+						c1.logout(null);
 					}
-				}else{			
-					c1.logout(null);
-				}
-			}else{
-				//menu do cliente
-				System.out.println(menuCliente());
-				if((n = lerint()) == 1) {
-					System.out.println("=========Solicitar Viagem=========");
-					if (c1.getEmail() != null) {
-						System.out.println("Local de Partida");
-						Local partida = lerLocal();
-						System.out.println("Local de Destino");
-						Local destino = lerLocal();
-						c1.solicitarViagem(c1.getEmail(), partida, destino);
+				} else {
+					// menu do cliente
+					System.out.println(menuCliente());
+					if ((n = lerint()) == 1) {
+						System.out.println("=========Solicitar Viagem=========");
+						if (c1.getEmail() != null) {
+							System.out.println("Local de Partida");
+							Local partida = lerLocal();
+							System.out.println("Local de Destino");
+							Local destino = lerLocal();
+							c1.solicitarViagem(c1.getEmail(), partida, destino);
+						} else {
+							System.out.println("Necessita de estar autenticado");
+						}
 					} else {
-						System.out.println("Necessita de estar autenticado");
+						c1.logout(null);
 					}
-				}else{
-					c1.logout(null);
 				}
 			}
+			System.out.println(menuInicio());
 		}
 	}
 }
