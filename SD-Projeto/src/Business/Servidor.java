6940161202cb38dc2ab0bfebf6bd583d04e591a9
PPinto22 <pedropinto24@hotmail.com>
2015-12-30 12:09:13 +0000

    .

--- SD-Projeto/src/Business/Servidor.java
+++ SD-Projeto/src/Business/Servidor.java
@@ -5,10 +5,46 @@
  */
 package Business;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import testesJMS.Boleias;
+import testesJMS.ServerConnection;
+
 /**
  *
  * @author ruifreitas
  */
 public class Servidor {
     
+	private ServerSocket sSock;
+	private int port;
+	private UMinhoBoleias boleias;
+	
+	public Servidor(int port) throws IOException {
+		this.port = port;
+		this.boleias = new UMinhoBoleias();
+		this.sSock = new ServerSocket(port);
+	}
+	
+	public void startServer(){
+		while(true){
+            Socket sock = null;
+            try {
+                sock = sSock.accept();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            Thread t;
+			try {
+				t = new Thread(new ServerConnection(sock,bol));
+				t.start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+        }
+	}
 }
